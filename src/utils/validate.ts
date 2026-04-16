ffcb15ba96f6ccd4de4368c94cb7c36142c0a64f
疯狂的狮子Li <15040126243@163.com>
2026-04-16 16:10:16 +0800

    update 优化 白名单支持对通配符路径匹配

--- src/utils/validate.ts
+++ src/utils/validate.ts
@@ -6,9 +6,10 @@
  */
 export function isPathMatch(pattern: string, path: string) {
   const regexPattern = pattern
-    .replace(/\//g, '\\/')
+    .replace(/([.+^${}()|\[\]\\])/g, '\\$1')
     .replace(/\*\*/g, '__DOUBLE_STAR__')
     .replace(/\*/g, '[^\\/]*')
+    .replace(/\?/g, '[^\\/]')
     .replace(/__DOUBLE_STAR__/g, '.*');
   const regex = new RegExp(`^${regexPattern}$`);
   return regex.test(path);

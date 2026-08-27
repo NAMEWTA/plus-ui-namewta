2b89848c00477475f2c541feaa4a598a5b9de043
wta <412751231@qq.com>
2026-08-27 20:09:24 +0800

    chore(types): regenerate admin component declarations

--- apps/admin-web/src/types/components.d.ts
+++ apps/admin-web/src/types/components.d.ts
@@ -14,6 +14,7 @@ declare module 'vue' {
     Breadcrumb: typeof import('./../components/Breadcrumb/index.vue')['default']
     DictTag: typeof import('./../components/DictTag/index.vue')['default']
     Editor: typeof import('./../components/Editor/index.vue')['default']
+    ElAlert: typeof import('element-plus/es')['ElAlert']
     ElBadge: typeof import('element-plus/es')['ElBadge']
     ElBreadcrumb: typeof import('element-plus/es')['ElBreadcrumb']
     ElBreadcrumbItem: typeof import('element-plus/es')['ElBreadcrumbItem']
